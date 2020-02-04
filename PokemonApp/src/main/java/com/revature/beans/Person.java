665b83d6fb9b4fb81de64ed89e562ac2247e6ea0
sierra <sierra.nicholes@revature.com>
2020-02-04 12:22:20 -0500

    alsdfknadsf

--- PokemonApp/src/main/java/com/revature/beans/Person.java
+++ PokemonApp/src/main/java/com/revature/beans/Person.java
@@ -7,8 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -60,12 +58,19 @@ public class Person {
 	public void setMoney(Integer money) {
 		this.money = money;
 	}
+	public List<Item> getItems() {
+		return items;
+	}
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((displayName == null) ? 0 : displayName.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((items == null) ? 0 : items.hashCode());
 		result = prime * result + ((money == null) ? 0 : money.hashCode());
 		result = prime * result + ((passwd == null) ? 0 : passwd.hashCode());
 		result = prime * result + ((username == null) ? 0 : username.hashCode());
@@ -90,6 +95,11 @@ public class Person {
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
+		if (items == null) {
+			if (other.items != null)
+				return false;
+		} else if (!items.equals(other.items))
+			return false;
 		if (money == null) {
 			if (other.money != null)
 				return false;
@@ -110,6 +120,6 @@ public class Person {
 	@Override
 	public String toString() {
 		return "Person [id=" + id + ", username=" + username + ", passwd=" + passwd + ", displayName=" + displayName
-				+ ", money=" + money + "]";
-	}	
+				+ ", money=" + money + ", items=" + items + "]";
+	}
 }
